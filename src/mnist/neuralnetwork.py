b6f88cde1680fec880839c2ce4555f84f1b77291
RikitoNoto <56541594+RikitoNoto@users.noreply.github.com>
2024-01-28 18:28:16 +0900

    create neuralnetwork gradient

--- src/mnist/neuralnetwork.py
+++ src/mnist/neuralnetwork.py
@@ -1,20 +1,22 @@
 # coding: utf-8
 import sys, os
 from typing import Callable
+from numbers import Number
 sys.path.append(os.pardir)  # 親ディレクトリのファイルをインポートするための設定
 from common.functions import *
 from common.gradient import numerical_gradient
 import numpy as np
+from numpy.typing import NDArray
 
 
 class NeuralNetwork:
-    def __init__(self, layer_count: int, layer_sizes: np.ndarray, activation_funcs: list[Callable[[np.ndarray], np.ndarray]] = None, weight_init_std=0.01):
+    def __init__(self, layer_count: int, layer_sizes: NDArray, activation_funcs: list[Callable[[NDArray], NDArray]] = None, weight_init_std=0.01):
         self.__activation_funcs = activation_funcs
         if not self.__activation_funcs:
             self.__activation_funcs = [sigmoid if i == layer_count-1 else softmax  for i in range(layer_count)]
         
         # 重みの初期化
-        self.params = []
+        self.params: list[NDArray] = []
         # 入力層があるのでlayerよりひとつ層の数が多くなる
         if layer_count + 1 != len(layer_sizes):
             raise ValueError("layer_sizesはlayer_count+1のサイズである必要があります。")
@@ -24,29 +26,29 @@ class NeuralNetwork:
             # 最後の行にバイアスを追加
             layer = np.vstack([layer, np.zeros(layer_sizes[i+1])])
             self.params.append(layer)
-
-    def predict(self, inputs: np.ndarray):
+            
+    def predict(self, inputs: NDArray):
         x =  inputs
         for i, weights in enumerate(self.params):
-            x = np.append(x, 1) # 最後の行にバイアスを追加
+            x = np.vstack([x, np.full((x.shape[1]), 1)]) # 最後の列にバイアスを追加
             a = np.dot(x, weights)
             y = self.__activation_funcs[i](a)
             x = y
         return y
         
-    # # x:入力データ, t:教師データ
-    # def loss(self, x, t):
-    #     y = self.predict(x)
+    # x:入力データ, t:教師データ
+    def loss(self, x, t):
+        y = self.predict(x)
         
-    #     return cross_entropy_error(y, t)
+        return cross_entropy_error(y, t)
     
-    # def accuracy(self, x, t):
-    #     y = self.predict(x)
-    #     y = np.argmax(y, axis=1)
-    #     t = np.argmax(t, axis=1)
+    def accuracy(self, x, t):
+        y = self.predict(x)
+        y = np.argmax(y, axis=1)
+        t = np.argmax(t, axis=1)
         
-    #     accuracy = np.sum(y == t) / float(x.shape[0])
-    #     return accuracy
+        accuracy = np.sum(y == t) / float(x.shape[0])
+        return accuracy
         
     # # x:入力データ, t:教師データ
     # def numerical_gradient(self, x, t):
@@ -59,8 +61,39 @@ class NeuralNetwork:
     #     grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
         
     #     return grads
-        
+    
+    def update_weights(self, gradients: list[NDArray], learning_rate = 0.1):
+        for i, layer_weights in enumerate(self.params):
+            self.params[i] = layer_weights - (learning_rate * gradients[i])
+
+    def gradient(self, x, t):
+        loss_W = lambda W: self.loss(x, t)
+        grads: list[NDArray] = []
+        for weights in self.params:
+            grads.append(self.partial_difference(loss_W, weights))
+        return grads
+    
+    def _center_difference(self, func: Callable[[Number], Number], x: Number, h: Number = 1e-4) -> Number:
+        diff_plus = func(x + h)
+        diff_minus = func(x - h)
+        return (diff_plus - diff_minus) / (2 * h)
+    
+    def partial_difference(self, func: Callable[[NDArray], Number], x: NDArray, h: Number = 1e-4) -> Number:
+        grad = np.zeros_like(x)
+        iterator = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+        while not iterator.finished:
+            idx = iterator.multi_index
+            def wrap(z) -> Number:
+                x_copy = x.copy()
+                x_copy[idx] = z
+                return func(x_copy)
+            grad[idx] = self._center_difference(wrap, x[idx], h)
+            iterator.iternext()
+        return grad
+    
     # def gradient(self, x, t):
+        
+        
     #     W1, W2 = self.params['W1'], self.params['W2']
     #     b1, b2 = self.params['b1'], self.params['b2']
     #     grads = {}
@@ -84,4 +117,4 @@ class NeuralNetwork:
     #     grads['b1'] = np.sum(da1, axis=0)
 
     #     return grads
-NeuralNetwork(3, np.array([3, 4, 3, 2])).predict(np.array([2,3,1]))
+# NeuralNetwork(3, np.array([3, 4, 3, 2])).predict(np.array([2,3,1]))
--- src/mnist/train_neuralnet.py
+++ src/mnist/train_neuralnet.py
@@ -1,5 +1,7 @@
 # coding: utf-8
 import sys, os
+
+from neuralnetwork import NeuralNetwork
 sys.path.append(os.pardir)  # 親ディレクトリのファイルをインポートするための設定
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,11 +11,47 @@ from two_layer_net import TwoLayerNet
 # データの読み込み
 (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
 
-network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+# network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
 
-iters_num = 10000  # 繰り返しの回数を適宜設定する
+# iters_num = 10000  # 繰り返しの回数を適宜設定する
+# train_size = x_train.shape[0]
+# batch_size = 100
+# learning_rate = 0.1
+
+# train_loss_list = []
+# train_acc_list = []
+# test_acc_list = []
+
+# iter_per_epoch = max(train_size / batch_size, 1)
+
+# for i in range(iters_num):
+#     batch_mask = np.random.choice(train_size, batch_size)
+#     x_batch = x_train[batch_mask]
+#     t_batch = t_train[batch_mask]
+    
+#     # 勾配の計算
+#     #grad = network.numerical_gradient(x_batch, t_batch)
+#     grad = network.gradient(x_batch, t_batch)
+    
+#     # パラメータの更新
+#     for key in ('W1', 'b1', 'W2', 'b2'):
+#         network.params[key] -= learning_rate * grad[key]
+    
+#     loss = network.loss(x_batch, t_batch)
+#     train_loss_list.append(loss)
+    
+#     if i % iter_per_epoch == 0:
+#         train_acc = network.accuracy(x_train, t_train)
+#         test_acc = network.accuracy(x_test, t_test)
+#         train_acc_list.append(train_acc)
+#         test_acc_list.append(test_acc)
+#         print("train acc, test acc | " + str(train_acc) + ", " + str(test_acc))
+
+network = NeuralNetwork(layer_count=2, layer_sizes=np.array([784, 50, 10]))
+
+iters_num = 1000  # 繰り返しの回数を適宜設定する
 train_size = x_train.shape[0]
-batch_size = 100
+batch_size = 10
 learning_rate = 0.1
 
 train_loss_list = []
@@ -32,18 +70,21 @@ for i in range(iters_num):
     grad = network.gradient(x_batch, t_batch)
     
     # パラメータの更新
-    for key in ('W1', 'b1', 'W2', 'b2'):
-        network.params[key] -= learning_rate * grad[key]
+    # for key in ('W1', 'b1', 'W2', 'b2'):
+    #     network.params[key] -= learning_rate * grad[key]
+    network.update_weights(grad)
     
     loss = network.loss(x_batch, t_batch)
     train_loss_list.append(loss)
     
-    if i % iter_per_epoch == 0:
+    if i % iter_per_epoch == 0 or True:
         train_acc = network.accuracy(x_train, t_train)
         test_acc = network.accuracy(x_test, t_test)
         train_acc_list.append(train_acc)
         test_acc_list.append(test_acc)
         print("train acc, test acc | " + str(train_acc) + ", " + str(test_acc))
+
+
 
 # グラフの描画
 markers = {'train': 'o', 'test': 's'}
