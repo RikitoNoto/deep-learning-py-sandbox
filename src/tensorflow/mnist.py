a8883c2cb6c36845935fd0309d5ed2fb153a8ca3
RikitoNoto <56541594+RikitoNoto@users.noreply.github.com>
2024-02-11 19:53:43 +0900

    tensorflow test

--- src/tensorflow/mnist.py
+++ src/tensorflow/mnist.py
@@ -8,8 +8,20 @@ x_train, x_test = x_train / 255.0, x_test / 255.0
 model = tf.keras.models.Sequential([
   tf.keras.layers.Flatten(input_shape=(28, 28)),
   tf.keras.layers.Dense(128, activation='relu'),
+  # 97.53 => 97.69
+  tf.keras.layers.Dense(128, activation='relu'),
+  # 97.69 => 97.77
+  tf.keras.layers.Dense(128, activation='relu'),
+  # 97.69 => 97.77
+  tf.keras.layers.Dense(128, activation='relu'),
+  # 97.69 => 97.77
+  tf.keras.layers.Dense(128, activation='relu'),
+  # 97.69 => 97.77
+  tf.keras.layers.Dense(128, activation='relu'),
+  # 97.69 => 97.77
+  tf.keras.layers.Dense(128, activation='relu'),
   tf.keras.layers.Dropout(0.2),
-  tf.keras.layers.Dense(10)
+  tf.keras.layers.Dense(10),
 ])
 predictions = model(x_train[:1]).numpy()
 print(predictions)
@@ -20,7 +32,7 @@ loss_fn(y_train[:1], predictions).numpy()
 model.compile(optimizer='adam',
               loss=loss_fn,
               metrics=['accuracy'])
-model.fit(x_train, y_train, epochs=5)
+model.fit(x_train, y_train, epochs=10)
 model.evaluate(x_test,  y_test, verbose=2)
 
 
