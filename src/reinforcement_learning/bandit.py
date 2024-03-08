96fd0a57e0682bbe7eda8aaddebc6cc5bf0d6750
R.Noto <rikitonoto@gmail.com>
2024-03-09 01:16:05 +0900

    create Agent

--- src/reinforcement_learning/bandit.py
+++ src/reinforcement_learning/bandit.py
@@ -1,11 +1,12 @@
+from typing import Optional
 import numpy as np
 
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 
 class Bandit:
-    def __init__(self):
-        self.__rate = np.random.rand()
+    def __init__(self, rate: Optional[float] = None):
+        self.__rate = rate or np.random.rand()
 
     @property
     def rate(self) -> float:
@@ -17,16 +18,52 @@ class Bandit:
         return 0
 
 
+class Agent:
+    def __init__(self, epsilon, action_size=10):
+        self.epsilon = epsilon
+        self.Qs = np.zeros(action_size)
+        self.ns = np.zeros(action_size)
+
+    def update(self, action, reward):
+        self.ns[action] += 1
+        self.Qs[action] += (reward - self.Qs[action]) / self.ns[action]
+
+    def get_action(self, n: int, steps: int):
+        if np.random.rand() < self.epsilon:
+            return np.random.randint(0, len(self.Qs))
+        return np.argmax(self.Qs)
+
+
 if __name__ == "__main__":
-    bandits = [Bandit() for _ in range(10)]
-    Qs = np.zeros(10)
-    ns = np.zeros(10)
 
-    [print(bandits[i].rate) for i in range(10)]
+    def run(steps: int, epsilon: float) -> tuple[int, list[int], list[float]]:
+        # bandits = [Bandit(i / 10 + i / 100) for i in range(10)]
+        bandits = [Bandit() for _ in range(10)]
+        agent = Agent(epsilon, 10)
+        total_reward = 0
+        total_rewards = []
+        rates = []
 
-    for n in range(10):
-        action = np.random.randint(0, 10)
-        reward = bandits[action].play()
-        ns[action] += 1
-        Qs[action] += (reward - Qs[action]) / ns[action]
-        print(f"No: {action}, count: {ns[action]}, Q: {Qs[action]}")
+        for step in range(steps):
+            action = agent.get_action(step, steps)
+            reward = bandits[action].play()
+            agent.update(action, reward)
+            total_reward += reward
+
+            total_rewards.append(total_reward)
+            rates.append(total_reward / (step + 1))
+        return total_reward, total_rewards, rates
+
+    runs = 200
+    steps = 1000
+    epsilon = 0.1
+    all_rates = np.zeros((runs, steps))
+    for i in range(runs):
+        total_reward, total_rewards, rates = run(steps, epsilon)
+        all_rates[i] = rates
+
+    avg_rates = np.average(all_rates, axis=0)
+    plt.ylabel("Rates")
+    plt.xlabel("Steps")
+    plt.plot(avg_rates)
+    plt.show()
