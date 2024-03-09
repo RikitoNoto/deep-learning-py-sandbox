18b5297428921325f8617d329465dd1bb333e674
RikitoNoto <56541594+RikitoNoto@users.noreply.github.com>
2024-03-09 18:43:49 +0900

    方策評価関数の実装

--- src/reinforcement_learning/environment.py
+++ src/reinforcement_learning/environment.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from enum import auto, Enum
 from typing import Optional
 import numpy as np
@@ -5,21 +6,21 @@ from reinforcement_learning.renderer import Renderer
 
 
 class Actions(Enum):
-    UP = auto()
-    DOWN = auto()
-    LEFT = auto()
-    RIGHT = auto()
+    UP = 0
+    DOWN = 1
+    LEFT = 2
+    RIGHT = 3
 
 
 class GridWorld:
     def __init__(self):
-        self.action_space = [0, 1, 2, 3]
-        self.action_meaning = {
-            0: "UP",
-            1: "DOWN",
-            2: "LEFT",
-            3: "RIGHT",
-        }
+        # self.action_space = [0, 1, 2, 3]
+        # self.action_meaning = {
+        #     0: "UP",
+        #     1: "DOWN",
+        #     2: "LEFT",
+        #     3: "RIGHT",
+        # }
 
         self.reward_map = np.array([[0, 0, 0, 1.0], [0, None, 0, -1.0], [0, 0, 0, 0]])
         self.goal_state = (0, 3)
@@ -40,16 +41,16 @@ class GridWorld:
         return self.reward_map.shape
 
     def actions(self):
-        return self.action_space
+        return [action for action in Actions]
 
     def states(self):
         for h in range(self.height):
             for w in range(self.width):
                 yield (h, w)
 
-    def next_state(self, state, action):
+    def next_state(self, state, action: Actions):
         action_move_map = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-        move = action_move_map[action]
+        move = action_move_map[action.value]
         next_state = (state[0] + move[0], state[1] + move[1])
         ny, nx = next_state
 
@@ -76,9 +77,33 @@ class GridWorld:
         self.agent_state = next_state
         return next_state, reward, done
 
-    def render_v(self, v=None, policy=None, print_value=True):
+    def render_v(
+        self,
+        v=None,
+        policy: Optional[defaultdict[tuple[int, int], dict[Actions, float]]] = None,
+        print_value=True,
+    ):
         renderer = Renderer(self.reward_map, self.goal_state, self.wall_state)
-        renderer.render_v(v, policy, print_value)
+
+        converted_policy: Optional[dict[tuple[int, int], dict[int, float]]] = None
+        if policy:
+            converted_policy = self.__convert_policy_actions_to_index(policy)
+
+        renderer.render_v(v, converted_policy, print_value)
+
+    def __convert_policy_actions_to_index(
+        self, policy: defaultdict[tuple[int, int], dict[Actions, float]]
+    ) -> defaultdict[tuple[int, int], dict[int, float]]:
+        converted_dict: dict[tuple[int, int], dict[int, float]] = defaultdict(
+            lambda: dict(
+                [(key.value, value) for key, value in policy.default_factory().items()]
+            )
+        )
+        for state, pi in policy.items():
+            converted_dict[state] = {}
+            for action, probability in pi.items():
+                converted_dict[state][action.value] = probability
+        return converted_dict
 
     def render_q(self, q=None, print_value=True):
         renderer = Renderer(self.reward_map, self.goal_state, self.wall_state)
